9dac7964e6721899420e97a91f3ed51d064ecc68
TianyuanHuuu <160672802+TianyuanHuuu@users.noreply.github.com>
2025-03-17 21:01:30 -0400

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -4,14 +4,30 @@ import json
 def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
 	#YOUR CODE HERE
-	json_data = json.dumps(data)
-	cid = ipfs.add_bytes(json_data.encode('utf-8'))['Hash']
+	# Pin JSON data to IPFS via Pinata API (no ipfs client required)
+        url = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
+    
+        headers = {
+            "Content-Type": "application/json"
+        # If Pinata gave you API keys, add: "Authorization": f"Bearer {PINATA_JWT}"
+        }
+    
+        payload = {
+            "pinataContent": data
+        }
+    
+	response = requests.post(url, headers=headers, json=payload)
+	response.raise_for_status()
+	    
+	cid = response.json()['IpfsHash']  # <-- that's the returned CID
 	return cid
 
 def get_from_ipfs(cid,content_type="json"):
 	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
 	#YOUR CODE HERE	
-	data = ipfs.cat(cid).decode('utf-8')
-	data = json.loads(data)
+	url = f"https://gateway.pinata.cloud/ipfs/{cid}"
+	response = requests.get(url)
+    	response.raise_for_status()
+	data = response.json()
 	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
 	return data
